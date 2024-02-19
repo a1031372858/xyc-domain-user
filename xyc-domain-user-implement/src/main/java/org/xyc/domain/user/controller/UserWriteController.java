29453b7d5e6642015cd1bfb47cfe7f7ac7037fe3
xuyachang <1031372858@qq.com>
2024-02-19 16:09:14 +0800

    新增注册逻辑

--- xyc-domain-user-implement/src/main/java/org/xyc/domain/user/controller/UserWriteController.java
+++ xyc-domain-user-implement/src/main/java/org/xyc/domain/user/controller/UserWriteController.java
@@ -1,12 +1,28 @@
 package org.xyc.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xyc.domain.base.model.Response;
+import org.xyc.domain.user.model.to.UserTO;
+import org.xyc.domain.user.service.UserService;
 
 /**
  * @author xuyachang
  * @date 2024/2/11
  */
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("api/user/write")
 public class UserWriteController {
+
+    private final UserService userService;
+
+    @PostMapping("register")
+    public Response<Boolean> register(UserTO userTO){
+        return userService.register(userTO);
+    }
+
 }
--- xyc-domain-user-implement/src/main/java/org/xyc/domain/user/service/UserService.java
+++ xyc-domain-user-implement/src/main/java/org/xyc/domain/user/service/UserService.java
@@ -3,11 +3,15 @@ package org.xyc.domain.user.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.xyc.domain.base.model.Response;
 import org.xyc.domain.user.converter.UserConverter;
 import org.xyc.domain.user.mapper.UserPOMapper;
 import org.xyc.domain.user.model.po.UserPO;
 import org.xyc.domain.user.model.to.UserTO;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 /**
  * @author xuyachang
@@ -25,5 +29,21 @@ public class UserService {
         userQueryWrapper.eq("phone",phone);
         UserPO userPO = userPOMapper.selectOne(userQueryWrapper);
         return Response.success(userConverter.convertT(userPO));
+    }
+
+
+    public Response<Boolean> register(UserTO userTO){
+        UserPO userPO = new UserPO();
+        userPO.setUsername("new_user" + userTO.getPhone());
+        userPO.setPhone(userTO.getPhone());
+        if(Objects.nonNull(userTO.getPassword())){
+            userPO.setPassword(DigestUtils.md5DigestAsHex(userTO.getPassword().getBytes(StandardCharsets.UTF_8)));
+        }else{
+            //没有密码时密码为空，无法使用密码登录
+            userPO.setPassword(userTO.getPassword());
+        }
+        userPOMapper.insert(userPO);
+
+        return Response.success(Boolean.TRUE);
     }
 }
