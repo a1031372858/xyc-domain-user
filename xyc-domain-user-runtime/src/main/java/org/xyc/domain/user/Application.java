685646e891b02244584eeeedb320a2c881904d4e
xuyachang <1031372858@qq.com>
2024-05-12 13:00:37 +0800

    引入spring-cloud-nacos依赖，注册到Nacos注册中心

--- xyc-domain-user-runtime/src/main/java/org/xyc/domain/user/Application.java
+++ xyc-domain-user-runtime/src/main/java/org/xyc/domain/user/Application.java
@@ -2,11 +2,13 @@ package org.xyc.domain.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
